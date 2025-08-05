cec192cea0344bbe02165baae2eaf4a4032eb046
Gabriel <glima9719@gmail.com>
2025-08-05 16:23:29 -0300

    feat: create todo entity

---
+++ src/main/java/br/com/gabriellima/desafio_todolist/entity/Todo.java
@@ -0,0 +1,56 @@
+package br.com.gabriellima.desafio_todolist.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+}
